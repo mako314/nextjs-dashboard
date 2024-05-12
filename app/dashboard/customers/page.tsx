34a4b6679cbff258139c6b18168ad6ba1f796291
Macolister <bispo.mac@gmail.com>
2024-05-12 14:14:52 -0400

    feat: finish metadata and tutorial

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Empty Customers',
+  description: 'Huh!',
+};
+
 export default function Page() {
     return <p>Customers Page</p>;
   }
